6d28bd5518343f8221470d1b7dc30d9fea0bf4f7
habiba2012 <habibaakhter2012@gmail.com>
2021-07-04 23:34:33 +0200

    first commit

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  authenticated = false;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+  
+  }
+
+  logout(): void {
+    this.http.post('http://localhost:8000/api/logout', {}, {withCredentials: true})
+      .subscribe(() => this.authenticated = false);
+  }
+
+}
